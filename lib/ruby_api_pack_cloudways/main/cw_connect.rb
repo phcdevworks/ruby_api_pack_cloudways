44e4a90c9ae1e9c5e033adfa9f17b29a341608c1
BradPotts <brad.potts@phcnet.co>
2021-05-24 11:15:14 +0000

    token request done.

---
+++ lib/ruby_api_pack_cloudways/main/cw_connect.rb
@@ -0,0 +1,30 @@
+module RubyApiPackCloudways
+    module Main
+        class CwConnect
+
+            # Connection - Attributes
+            attr_accessor :cw_api_url_base, :cw_api_path, :cw_api_token
+
+            # Connection - Init
+            def initialize(cw_api_url_base, cw_api_path, cw_api_token)  
+                @cw_api_url_base = cw_api_url_base
+                @cw_api_path = cw_api_path
+                @cw_api_token = cw_api_token
+            end 
+
+            # Connection - API Connection Call
+            def cloudways_api_connection
+
+                cw_api_connetion_response = Faraday.new(url: @cw_api_url_base) do |cw_api_connection|
+                    cw_api_connection.request :oauth2, cw_api_token, token_type: :bearer
+                    cw_api_connection.response :logger
+                    cw_api_connection.adapter Faraday.default_adapter
+                end
+
+                return cw_api_connetion_response.get @cw_api_url_base + @cw_api_path
+
+            end
+
+        end            
+    end         
+end
