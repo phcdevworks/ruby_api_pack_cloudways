5cbfab95e0a8dfe887dc06488f938cdfd8cfed2c
Bradley J Potts <brad.potts@coastdigitalgroup.com>
2021-06-03 15:05:08 +0000

    changes to make it work with external apps.

---
+++ lib/ruby_api_pack_cloudways/cw_token.rb
@@ -0,0 +1,42 @@
+module RubyApiPackCloudways
+    class CwToken
+
+        # Cloudways - Token - Init
+        def initialize(cw_api_url_base, cw_url_path_auth, cw_user_email, cw_user_key)  
+            @cw_api_url_base = cw_api_url_base
+            @cw_url_path_auth = cw_url_path_auth
+            @cw_user_email = cw_user_email
+            @cw_user_key = cw_user_key
+        end 
+
+        # Cloudways - Token - Connection
+        def  self.cw_api_token_connection
+
+            # Cloudways - Token - Connection via Faraday
+            Faraday.new url: @cw_api_url_base + @cw_url_path_auth do |cw_token_connection|
+                cw_token_connection.request  :url_encoded
+                cw_token_connection.response :logger
+                cw_token_connection.adapter  Faraday.default_adapter
+            end
+
+        end
+
+        # Cloudways - Token - Request
+        def self.cw_api_token
+
+            # Cloudways - Token - Request From Above Connection Method
+            cloudways_token_request = cw_api_token_connection.post do |cw_token_request|
+                cw_token_request.headers["Content-Type"] = "application/x-www-form-urlencoded"
+                cw_token_request.body = {
+                    email: URI::encode(@cw_user_email),
+                    api_key: URI::encode(@cw_user_key)
+                }
+            end
+
+            # Cloudways - Token - Request Isolate and Get Token from Response via OJ
+            return @cw_apit_token = Oj.load(cloudways_token_request.body)["access_token"]
+
+        end
+
+    end
+end
