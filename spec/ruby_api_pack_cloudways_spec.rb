42981a883a10049e2487d7d764a2e057fe6ed702
Bradley J Potts <brad.potts@coastdigitalgroup.com>
2024-07-04 12:52:28 +0800

    setup rspec

--- spec/ruby_api_pack_cloudways_spec.rb
+++ spec/ruby_api_pack_cloudways_spec.rb
@@ -1,14 +1,9 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
-require 'ruby_api_pack_cloudways'
 
 RSpec.describe RubyApiPackCloudways do
   it 'has a version number' do
     expect(RubyApiPackCloudways::VERSION).not_to be_nil
-  end
-
-  it 'initializes the CwClient class' do
-    expect { RubyApiPackCloudways::CwClient.new }.not_to raise_error
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,13 +4,22 @@ require 'bundler/setup'
 require 'ruby_api_pack_cloudways'
 
 RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = '.rspec_status'
-
-  # Disable RSpec exposing methods globally on `Module` and `main`
-  config.disable_monkey_patching!
-
-  config.expect_with :rspec do |c|
-    c.syntax = :expect
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+
+  config.disable_monkey_patching!
+  config.warnings = true
+
+  config.default_formatter = 'doc' if config.files_to_run.one?
+
+  config.order = :random
+
+  Kernel.srand config.seed
 end
