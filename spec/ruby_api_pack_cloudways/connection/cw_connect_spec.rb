c21a426feddcde3daca340f02ca9cfcaa4c97fb2
Bradley J Potts <brad.potts@coastdigitalgroup.com>
2024-07-05 11:14:38 +0800

    rspec and rubocop clean up with no failures

--- spec/ruby_api_pack_cloudways/connection/cw_connect_spec.rb
+++ spec/ruby_api_pack_cloudways/connection/cw_connect_spec.rb
@@ -7,27 +7,33 @@ require 'ruby_api_pack_cloudways/constants'
 RSpec.describe RubyApiPackCloudways::Connection::CwConnect do
   include RubyApiPackCloudways::Constants
 
-  subject(:connection) { described_class.new(cw_api_url_base, cw_api_path, mock_faraday) }
+  subject(:connection) { described_class.new(connection_params[:url_base], connection_params[:path], mock_faraday) }
 
-  let(:cw_api_url_base) { RubyApiPackCloudways::Constants::CW_API_URL }
-  let(:cw_api_path) { '/some_path' }
+  let(:connection_params) do
+    {
+      url_base: RubyApiPackCloudways::Constants::CW_API_URL,
+      path: '/some_path'
+    }
+  end
   let(:mock_token) { 'fake_token' }
   let(:response_body) { '{"data":"value"}' }
 
   let(:mock_faraday) { class_double(Faraday) }
+  let(:mock_connection) { instance_double(Faraday::Connection) }
 
   before do
     allow(RubyApiPackCloudways::Connection::CwToken).to receive(:new)
       .and_return(instance_double(RubyApiPackCloudways::Connection::CwToken, cw_api_token: mock_token))
-    allow(mock_faraday).to receive(:new).and_return(instance_double(Faraday::Connection,
-                                                                    get: instance_double(Faraday::Response,
-                                                                                         status: 200, body: response_body)))
+    allow(mock_faraday).to receive(:new).and_return(
+      instance_double(Faraday::Connection,
+                      get: instance_double(Faraday::Response, status: 200, body: response_body))
+    )
   end
 
   describe '#cloudways_api_connection' do
     it 'creates a Faraday connection' do
       connection.cloudways_api_connection
-      expect(mock_faraday).to have_received(:new).with(url: cw_api_url_base)
+      expect(mock_faraday).to have_received(:new).with(url: connection_params[:url_base])
     end
 
     it 'returns a successful response' do
--- spec/ruby_api_pack_cloudways/connection/cw_token_spec.rb
+++ spec/ruby_api_pack_cloudways/connection/cw_token_spec.rb
@@ -7,14 +7,20 @@ require 'ruby_api_pack_cloudways/constants'
 RSpec.describe RubyApiPackCloudways::Connection::CwToken do
   include RubyApiPackCloudways::Constants
 
+  subject(:token) do
+    described_class.new(cw_api_url_base, cw_url_path_auth, credentials[:email], credentials[:key], mock_faraday)
+  end
+
   let(:cw_api_url_base) { RubyApiPackCloudways::Constants::CW_API_URL }
   let(:cw_url_path_auth) { RubyApiPackCloudways::Constants::CW_API_PATH_TOKEN }
-  let(:cw_user_email) { RubyApiPackCloudways::Constants::CW_API_EMAIL }
-  let(:cw_user_key) { RubyApiPackCloudways::Constants::CW_API_KEY }
+  let(:credentials) do
+    {
+      email: RubyApiPackCloudways::Constants::CW_API_EMAIL,
+      key: RubyApiPackCloudways::Constants::CW_API_KEY
+    }
+  end
   let(:mock_faraday) { class_double(Faraday) }
   let(:mock_connection) { instance_double(Faraday::Connection) }
-
-  subject(:token) { described_class.new(cw_api_url_base, cw_url_path_auth, cw_user_email, cw_user_key, mock_faraday) }
 
   describe '#cw_api_token_connection' do
     before do
@@ -35,15 +41,21 @@ RSpec.describe RubyApiPackCloudways::Connection::CwToken do
   end
 
   describe '#cw_api_token' do
-    it 'returns an access token' do
+    before do
       allow(mock_faraday).to receive(:new).and_return(mock_connection)
       allow(mock_connection).to receive(:post).and_return(
         instance_double(Faraday::Response, body: '{"access_token":"fake_token"}')
       )
+    end
 
+    it 'returns an access token' do
       token_result = token.cw_api_token
-
       expect(token_result).to eq('fake_token')
+    end
+
+    it 'calls the correct endpoint' do
+      token.cw_api_token
+      expect(mock_connection).to have_received(:post)
     end
   end
 end
