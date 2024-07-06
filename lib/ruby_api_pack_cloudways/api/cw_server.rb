9c06cc8fd5061cb169fd42450040439a564e4f67
Bradley J Potts <brad.potts@coastdigitalgroup.com>
2024-07-06 17:28:14 +0800

    add initializer config instead of env vars

--- lib/ruby_api_pack_cloudways/api/cw_server.rb
+++ lib/ruby_api_pack_cloudways/api/cw_server.rb
@@ -1,16 +1,12 @@
 # frozen_string_literal: true
 
-require_relative '../constants'
-
 module RubyApiPackCloudways
   module Api
     class CwServer
-      # PHCDEVONE - Include the RubyApiPackCloudways::Constants module
-      include RubyApiPackCloudways::Constants
-
-      # PHCDEVONE - Fetch the list of servers from the Cloudways API
+      # Fetch the list of servers from the Cloudways API
       def self.cw_server_list
-        servers = Connection::CwConnect.new(CW_API_URL, '/server').cloudways_api_connection
+        servers = Connection::CwConnect.new(RubyApiPackCloudways.configuration.api_url,
+                                            '/server').cloudways_api_connection
         servers['servers']
       end
     end
--- spec/ruby_api_pack_cloudways/api/cw_lists_spec.rb
+++ spec/ruby_api_pack_cloudways/api/cw_lists_spec.rb
@@ -2,69 +2,37 @@
 
 require 'spec_helper'
 require 'ruby_api_pack_cloudways/api/cw_lists'
-require 'ruby_api_pack_cloudways/constants'
 
-# PHCDEVONE - RSpec describe block for the RubyApiPackCloudways::Api::CwLists class
 RSpec.describe RubyApiPackCloudways::Api::CwLists do
-  # PHCDEVONE - Include the RubyApiPackCloudways::Constants module
-  include RubyApiPackCloudways::Constants
+  let(:mock_cw_connect) { instance_double(RubyApiPackCloudways::Connection::CwConnect) }
 
-  # PHCDEVONE - Define a mock Faraday class double
-  let(:mock_faraday) { class_double(Faraday) }
-
-  # PHCDEVONE - Define a connection instance for CwConnect with mock Faraday
-  let(:connection) do
-    RubyApiPackCloudways::Connection::CwConnect.new(RubyApiPackCloudways::Constants::CW_API_URL, '', mock_faraday)
-  end
-
-  # PHCDEVONE - Define a mock connection instance double for Faraday::Connection
-  let(:mock_connection) { instance_double(Faraday::Connection) }
-
-  # PHCDEVONE - Define the response body for the mock connection
-  let(:response_body) do
-    '{"providers":["provider1"],"sizes":["size1"],"apps":["app1"],"packages":["package1"]}'
-  end
-
-  # PHCDEVONE - Set up the mock Faraday and connection before each test
   before do
-    allow(mock_faraday).to receive(:new).and_return(mock_connection)
-    allow(mock_connection).to receive(:get).and_return(
-      instance_double(Faraday::Response, status: 200, body: response_body)
+    allow(RubyApiPackCloudways::Connection::CwConnect).to receive(:new).and_return(mock_cw_connect)
+    allow(mock_cw_connect).to receive(:cloudways_api_connection).and_return(
+      'providers' => ['provider1'], 'sizes' => ['size1'], 'apps' => ['app1'], 'packages' => ['package1']
     )
   end
 
-  # PHCDEVONE - Describe block for the .cw_provider_list method
   describe '.cw_provider_list' do
-    # PHCDEVONE - It block to test the return of provider list
     it 'returns a list of providers' do
-      allow(RubyApiPackCloudways::Connection::CwConnect).to receive(:new).and_return(connection)
       expect(described_class.cw_provider_list).to eq(['provider1'])
     end
   end
 
-  # PHCDEVONE - Describe block for the .cw_server_size_list method
   describe '.cw_server_size_list' do
-    # PHCDEVONE - It block to test the return of server sizes list
     it 'returns a list of server sizes' do
-      allow(RubyApiPackCloudways::Connection::CwConnect).to receive(:new).and_return(connection)
       expect(described_class.cw_server_size_list).to eq(['size1'])
     end
   end
 
-  # PHCDEVONE - Describe block for the .cw_app_list method
   describe '.cw_app_list' do
-    # PHCDEVONE - It block to test the return of apps list
     it 'returns a list of apps' do
-      allow(RubyApiPackCloudways::Connection::CwConnect).to receive(:new).and_return(connection)
       expect(described_class.cw_app_list).to eq(['app1'])
     end
   end
 
-  # PHCDEVONE - Describe block for the .cw_package_list method
   describe '.cw_package_list' do
-    # PHCDEVONE - It block to test the return of packages list
     it 'returns a list of packages' do
-      allow(RubyApiPackCloudways::Connection::CwConnect).to receive(:new).and_return(connection)
       expect(described_class.cw_package_list).to eq(['package1'])
     end
   end
