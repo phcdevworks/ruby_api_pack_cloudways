eb7fd9888ae5a9fda66d6484c00da72c473a1c60
Bradley J Potts <brad.potts@coastdigitalgroup.com>
2024-07-07 19:07:54 +0800

    fixed line ending issue

--- spec/ruby_api_pack_cloudways/connection/cw_connect_spec.rb
+++ spec/ruby_api_pack_cloudways/connection/cw_connect_spec.rb
@@ -7,18 +7,23 @@ RSpec.describe RubyApiPackCloudways::Connection::CwConnect do
   let(:connection) { described_class.new('https://api.cloudways.com/api/v1', '/some_path') }
 
   before do
-    allow(RubyApiPackCloudways::Connection::CwToken).to receive(:new).and_return(instance_double(RubyApiPackCloudways::Connection::CwToken, cw_api_token: 'fake_token'))
+    allow(RubyApiPackCloudways::Connection::CwToken).to receive(:new).and_return(instance_double(
+                                                                                   RubyApiPackCloudways::Connection::CwToken, cw_api_token: 'fake_token'
+                                                                                 ))
   end
 
   describe '#cloudways_api_connection' do
     it 'creates a HTTParty connection' do
-      allow(HTTParty).to receive(:get).and_return(instance_double(HTTParty::Response, code: 200, body: '{"data":"value"}'))
+      allow(HTTParty).to receive(:get).and_return(instance_double(HTTParty::Response, code: 200,
+                                                                                      body: '{"data":"value"}'))
       connection.cloudways_api_connection
-      expect(HTTParty).to have_received(:get).with('https://api.cloudways.com/api/v1/some_path', headers: { 'Authorization' => 'Bearer fake_token' })
+      expect(HTTParty).to have_received(:get).with('https://api.cloudways.com/api/v1/some_path',
+                                                   headers: { 'Authorization' => 'Bearer fake_token' })
     end
 
     it 'returns a successful response' do
-      allow(HTTParty).to receive(:get).and_return(instance_double(HTTParty::Response, code: 200, body: '{"data":"value"}'))
+      allow(HTTParty).to receive(:get).and_return(instance_double(HTTParty::Response, code: 200,
+                                                                                      body: '{"data":"value"}'))
       response = connection.cloudways_api_connection
       expect(response['data']).to eq('value')
     end
