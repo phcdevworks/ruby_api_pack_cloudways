4613be9250af4dc5eb102c74bffd325d25f02b1d
Bradley J Potts <brad.potts@coastdigitalgroup.com>
2024-07-08 10:22:45 +0800

    Fixed gem rspec main file, dry up cw_server code

--- spec/ruby_api_pack_cloudways_spec.rb
+++ spec/ruby_api_pack_cloudways_spec.rb
@@ -2,7 +2,7 @@
 
 require 'spec_helper'
 
-RSpec.describe 'RubyApiPackCloudways Integration Test', :vcr do
+RSpec.describe 'RubyApiPackCloudways', :vcr do
   before do
     RubyApiPackCloudways.configure do |config|
       config.api_url = ENV['CLOUDWAYS_API_URL'] || 'https://api.cloudways.com/api/v1'
@@ -12,31 +12,31 @@ RSpec.describe 'RubyApiPackCloudways Integration Test', :vcr do
     end
   end
 
-  describe 'CwServer' do
+  describe RubyApiPackCloudways::Api::CwServer do
     it 'fetches the list of servers' do
-      servers = RubyApiPackCloudways::Api::CwServer.cw_server_list
+      servers = described_class.cw_server_list
       expect(servers).to be_an(Array)
     end
   end
 
-  describe 'CwLists' do
+  describe RubyApiPackCloudways::Api::CwLists do
     it 'fetches the list of providers' do
-      providers = RubyApiPackCloudways::Api::CwLists.cw_provider_list
+      providers = described_class.cw_provider_list
       expect(providers).to be_an(Array)
     end
 
     it 'fetches the list of server sizes' do
-      sizes = RubyApiPackCloudways::Api::CwLists.cw_server_size_list
+      sizes = described_class.cw_server_size_list
       expect(sizes).to be_a(Hash)
     end
 
     it 'fetches the list of apps' do
-      apps = RubyApiPackCloudways::Api::CwLists.cw_app_list
+      apps = described_class.cw_app_list
       expect(apps).to be_a(Hash)
     end
 
     it 'fetches the list of packages' do
-      packages = RubyApiPackCloudways::Api::CwLists.cw_package_list
+      packages = described_class.cw_package_list
       expect(packages).to be_a(Hash)
     end
   end
