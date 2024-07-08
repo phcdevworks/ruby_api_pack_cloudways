54973cab80d653441d1857acd5368eab564be4e0
Bradley J Potts <brad.potts@coastdigitalgroup.com>
2024-07-08 09:30:31 +0800

    updating spec, and line ending fixes

--- spec/ruby_api_pack_cloudways/api/cw_server_spec.rb
+++ spec/ruby_api_pack_cloudways/api/cw_server_spec.rb
@@ -4,9 +4,15 @@ require 'spec_helper'
 require 'ruby_api_pack_cloudways/api/cw_server'
 
 RSpec.describe RubyApiPackCloudways::Api::CwServer do
+  let(:cw_connect_instance) { instance_double(RubyApiPackCloudways::Connection::CwConnect) }
+
+  before do
+    allow(RubyApiPackCloudways::Connection::CwConnect).to receive(:new).and_return(cw_connect_instance)
+    allow(cw_connect_instance).to receive(:cloudways_api_connection).and_return('servers' => ['server1'])
+  end
+
   describe '.cw_server_list' do
     it 'returns a list of servers' do
-      allow_any_instance_of(RubyApiPackCloudways::Connection::CwConnect).to receive(:cloudways_api_connection).and_return('servers' => ['server1'])
       servers = described_class.cw_server_list
       expect(servers).to eq(['server1'])
     end
--- spec/ruby_api_pack_cloudways_spec.rb
+++ spec/ruby_api_pack_cloudways_spec.rb
@@ -2,9 +2,9 @@
 
 require 'spec_helper'
 
-RSpec.describe 'RubyApiPackCloudways Integration Test', :vcr do
+RSpec.describe RubyApiPackCloudways, :vcr do
   before do
-    RubyApiPackCloudways.configure do |config|
+    described_class.configure do |config|
       config.api_url = ENV['CLOUDWAYS_API_URL'] || 'https://api.cloudways.com/api/v1'
       config.api_path_token = ENV['CLOUDWAYS_API_PATH_TOKEN'] || '/oauth/access_token'
       config.api_email = ENV['CLOUDWAYS_API_EMAIL']
@@ -12,32 +12,32 @@ RSpec.describe 'RubyApiPackCloudways Integration Test', :vcr do
     end
   end
 
-  describe 'CwServer' do
+  describe RubyApiPackCloudways::Api::CwServer do
     it 'fetches the list of servers' do
-      servers = RubyApiPackCloudways::Api::CwServer.cw_server_list
+      servers = described_class.cw_server_list
       expect(servers).to be_an(Array)
     end
   end
 
-  describe 'CwLists' do
+  describe RubyApiPackCloudways::Api::CwLists do
     it 'fetches the list of providers' do
-      providers = RubyApiPackCloudways::Api::CwLists.cw_provider_list
+      providers = described_class.cw_provider_list
       expect(providers).to be_an(Array)
     end
 
     it 'fetches the list of server sizes' do
-      sizes = RubyApiPackCloudways::Api::CwLists.cw_server_size_list
-      expect(sizes).to be_a(Hash)
+      sizes = described_class.cw_server_size_list
+      expect(sizes).to be_an(Array)
     end
 
     it 'fetches the list of apps' do
-      apps = RubyApiPackCloudways::Api::CwLists.cw_app_list
-      expect(apps).to be_a(Hash)
+      apps = described_class.cw_app_list
+      expect(apps).to be_an(Array)
     end
 
     it 'fetches the list of packages' do
-      packages = RubyApiPackCloudways::Api::CwLists.cw_package_list
-      expect(packages).to be_a(Hash)
+      packages = described_class.cw_package_list
+      expect(packages).to be_an(Array)
     end
   end
 end
