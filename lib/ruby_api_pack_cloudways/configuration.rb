6acad98140a083a33b26262048fae2068ee70c28
Bradley J Potts <brad.potts@coastdigitalgroup.com>
2024-07-10 18:45:54 +0800

    Started on the complete removal of Faraday gem, file clean up & comments

--- lib/ruby_api_pack_cloudways/configuration.rb
+++ lib/ruby_api_pack_cloudways/configuration.rb
@@ -1,13 +1,20 @@
 # frozen_string_literal: true
 
 module RubyApiPackCloudways
+  # PHCDEVONE - Define the Configuration class within the RubyApiPackCloudways module
   class Configuration
+    # PHCDEVONE - Define attribute accessors for the configuration settings
     attr_accessor :api_url, :api_path_token, :api_email, :api_key
 
+    # PHCDEVONE - Initialize the configuration with default values
     def initialize
+      # PHCDEVONE - Set the default API URL for Cloudways
       @api_url = 'https://api.cloudways.com/api/v1'
+      # PHCDEVONE - Set the default API path for token authentication
       @api_path_token = '/oauth/access_token'
+      # PHCDEVONE - Initialize the API email as nil
       @api_email = nil
+      # PHCDEVONE - Initialize the API key as nil
       @api_key = nil
     end
   end
--- lib/ruby_api_pack_cloudways/connection/cw_token.rb
+++ lib/ruby_api_pack_cloudways/connection/cw_token.rb
@@ -1,19 +1,25 @@
 # frozen_string_literal: true
 
-require 'httparty'
-
 module RubyApiPackCloudways
   module Connection
+    # PHCDEVONE - Define the CwToken class within the RubyApiPackCloudways::Connection module
     class CwToken
+      # PHCDEVONE - Define attribute readers for the API URL base, auth path, user email, and user key
       attr_reader :cw_api_url_base, :cw_url_path_auth, :cw_user_email, :cw_user_key
 
+      # PHCDEVONE - Initialize the CwToken with configuration settings
       def initialize
+        # PHCDEVONE - Set the base URL for the Cloudways API from the configuration
         @cw_api_url_base = RubyApiPackCloudways.configuration.api_url
+        # PHCDEVONE - Set the authentication path for the Cloudways API from the configuration
         @cw_url_path_auth = RubyApiPackCloudways.configuration.api_path_token
+        # PHCDEVONE - Set the user email for the Cloudways API from the configuration
         @cw_user_email = RubyApiPackCloudways.configuration.api_email
+        # PHCDEVONE - Set the user key for the Cloudways API from the configuration
         @cw_user_key = RubyApiPackCloudways.configuration.api_key
       end
 
+      # PHCDEVONE - Method to obtain the API token from the Cloudways API
       def cw_api_token
         response = HTTParty.post(
           "#{@cw_api_url_base}#{@cw_url_path_auth}",
@@ -25,6 +31,7 @@ module RubyApiPackCloudways
 
       private
 
+      # PHCDEVONE - Method to parse the response from the API
       def parse_response(response)
         Oj.load(response.body)
       rescue Oj::ParseError => e
