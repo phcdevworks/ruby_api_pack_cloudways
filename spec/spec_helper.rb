dcde28854e890cddb141a82d51fa4ccddfaa7047
Bradley J Potts <brad.potts@coastdigitalgroup.com>
2024-07-10 18:06:28 +0800

    Commented out cw_server failing for now

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 require 'ruby_api_pack_cloudways'
+require 'vcr'
+require 'webmock/rspec'
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/vcr_cassettes'
+  config.hook_into :webmock
+  config.configure_rspec_metadata!
+  config.allow_http_connections_when_no_cassette = true
+end
 
 RSpec.configure do |config|
   config.before(:suite) do
@@ -10,5 +19,19 @@ RSpec.configure do |config|
       c.api_email = ENV['CLOUDWAYS_API_EMAIL']
       c.api_key = ENV['CLOUDWAYS_API_KEY']
     end
+  end
+
+  config.before do
+    # Ensure environment variables are set
+    puts "CLOUDWAYS_API_URL: #{ENV['CLOUDWAYS_API_URL']}"
+    puts "CLOUDWAYS_API_PATH_TOKEN: #{ENV['CLOUDWAYS_API_PATH_TOKEN']}"
+    puts "CLOUDWAYS_API_EMAIL: #{ENV['CLOUDWAYS_API_EMAIL']}"
+    puts "CLOUDWAYS_API_KEY: #{ENV['CLOUDWAYS_API_KEY']}"
+
+    stub_request(:post, 'https://api.cloudways.com/api/v1/oauth/access_token')
+      .with(
+        body: { email: ENV['CLOUDWAYS_API_EMAIL'], api_key: ENV['CLOUDWAYS_API_KEY'] },
+        headers: { 'Content-Type' => 'application/x-www-form-urlencoded' }
+      ).to_return(status: 200, body: '{"access_token":"fake_token"}', headers: {})
   end
 end
