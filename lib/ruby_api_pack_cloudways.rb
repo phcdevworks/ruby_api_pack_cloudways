b41233ed03300e0b990192b434796875fb8157f0
Bradley J Potts <brad.potts@coastdigitalgroup.com>
2024-09-18 02:07:19 +0800

    update readme.

--- lib/ruby_api_pack_cloudways.rb
+++ lib/ruby_api_pack_cloudways.rb
@@ -1,27 +1,20 @@
 # frozen_string_literal: true
 
-# PHCDEVONE - Load Required Gems
 require 'httparty'
 require 'oj'
 
-# PHCDEVONE - Load Configuration and API Files
 require_relative 'ruby_api_pack_cloudways/configuration'
 require_relative 'ruby_api_pack_cloudways/connection/cw_token'
 require_relative 'ruby_api_pack_cloudways/connection/cw_connect'
 require_relative 'ruby_api_pack_cloudways/api/cw_lists'
 require_relative 'ruby_api_pack_cloudways/api/cw_server'
 
-# PHCDEVONE - Define the main module for the Ruby API Pack Cloudways
 module RubyApiPackCloudways
   class << self
-    # PHCDEVONE - Define an accessor for the configuration
     attr_accessor :configuration
 
-    # PHCDEVONE - Method to configure the gem
     def configure
-      # PHCDEVONE - Initialize the configuration if it hasn't been already
       self.configuration ||= Configuration.new
-      # PHCDEVONE - Yield the configuration to the block provided by the caller
       yield(configuration)
     end
   end
--- lib/ruby_api_pack_cloudways/api/cw_server.rb
+++ lib/ruby_api_pack_cloudways/api/cw_server.rb
@@ -2,14 +2,11 @@
 
 module RubyApiPackCloudways
   module Api
-    # PHCDEVONE - Define the CwServer class within the RubyApiPackCloudways::Api module
     class CwServer
-      # PHCDEVONE - Fetch the list of servers from the Cloudways API
       def self.cw_server_list
         fetch_list('/server')['servers']
       end
 
-      # PHCDEVONE - Fetch the list from the Cloudways API based on the given endpoint
       def self.fetch_list(endpoint)
         Connection::CwConnect.new(RubyApiPackCloudways.configuration.api_url, endpoint).cloudways_api_connection
       end
