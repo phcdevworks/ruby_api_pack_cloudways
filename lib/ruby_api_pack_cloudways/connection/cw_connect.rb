80a625fb28633e2febb674a27061b72cf7ebcea8
Bradley J Potts <brad.potts@coastdigitalgroup.com>
2024-10-30 11:47:42 +0800

    Add full server endpoints to CwServer, specs for GET & POST methods

--- lib/ruby_api_pack_cloudways/connection/cw_connect.rb
+++ lib/ruby_api_pack_cloudways/connection/cw_connect.rb
@@ -3,13 +3,16 @@
 module RubyApiPackCloudways
   module Connection
     class CwConnect
+      # Initialize Cloudways API URL and path
       attr_reader :cw_api_url_base, :cw_api_path
 
+      # Initialize Cloudways API URL and path
       def initialize(cw_api_url_base, cw_api_path)
         @cw_api_url_base = cw_api_url_base
         @cw_api_path = cw_api_path
       end
 
+      # GET request to Cloudways API
       def cloudways_api_connection
         token = CwToken.new.cw_api_token
         response = HTTParty.get(
@@ -19,8 +22,20 @@ module RubyApiPackCloudways
         handle_response(response)
       end
 
+      # POST request to Cloudways API
+      def cloudways_api_post_connection(params)
+        token = CwToken.new.cw_api_token
+        response = HTTParty.post(
+          "#{@cw_api_url_base}#{@cw_api_path}",
+          headers: { 'Authorization' => "Bearer #{token}", 'Content-Type' => 'application/json' },
+          body: params.to_json
+        )
+        handle_response(response)
+      end
+
       private
 
+      # Handle response from Cloudways API
       def handle_response(response)
         case response.code
         when 200
@@ -30,6 +45,7 @@ module RubyApiPackCloudways
         end
       end
 
+      # Parse response from Cloudways API
       def parse_response(response)
         Oj.load(response.body)
       rescue Oj::ParseError => e
--- spec/ruby_api_pack_cloudways/connection/cw_connect_spec.rb
+++ spec/ruby_api_pack_cloudways/connection/cw_connect_spec.rb
@@ -7,14 +7,16 @@ RSpec.describe RubyApiPackCloudways::Connection::CwConnect do
   let(:connection) { described_class.new('https://api.cloudways.com/api/v1', '/some_path') }
   let(:token_instance) { instance_double(RubyApiPackCloudways::Connection::CwToken, cw_api_token: 'fake_token') }
   let(:http_response) { instance_double(HTTParty::Response, code: 200, body: '{"data":"value"}') }
+  let(:post_params) { { key: 'value' } }
 
   before do
     allow(RubyApiPackCloudways::Connection::CwToken).to receive(:new).and_return(token_instance)
     allow(HTTParty).to receive(:get).and_return(http_response)
+    allow(HTTParty).to receive(:post).and_return(http_response)
   end
 
   describe '#cloudways_api_connection' do
-    it 'creates a HTTParty GET connection' do
+    it 'creates an HTTParty GET connection' do
       connection.cloudways_api_connection
 
       expect(HTTParty).to have_received(:get).with(
@@ -50,6 +52,53 @@ RSpec.describe RubyApiPackCloudways::Connection::CwConnect do
 
       it 'raises a parsing error' do
         expect { connection.cloudways_api_connection }.to raise_error(/Error parsing response: Unexpected character/)
+      end
+    end
+  end
+
+  describe '#cloudways_api_post_connection' do
+    it 'creates an HTTParty POST connection with JSON body' do
+      connection.cloudways_api_post_connection(post_params)
+
+      expect(HTTParty).to have_received(:post).with(
+        'https://api.cloudways.com/api/v1/some_path',
+        headers: {
+          'Authorization' => 'Bearer fake_token',
+          'Content-Type' => 'application/json'
+        },
+        body: post_params.to_json
+      )
+    end
+
+    it 'returns a successful response for POST request' do
+      response = connection.cloudways_api_post_connection(post_params)
+      expect(response['data']).to eq('value')
+    end
+
+    context 'when the POST response code is not 200' do
+      let(:error_response) { instance_double(HTTParty::Response, code: 500, body: '{"error":"Server error"}') }
+
+      before do
+        allow(HTTParty).to receive(:post).and_return(error_response)
+      end
+
+      it 'raises an error with the response code for POST request' do
+        expect { connection.cloudways_api_post_connection(post_params) }.to raise_error('Error: Received status 500')
+      end
+    end
+
+    context 'when parsing the POST response fails' do
+      let(:faulty_response) { instance_double(HTTParty::Response, code: 200, body: 'invalid_json') }
+
+      before do
+        allow(HTTParty).to receive(:post).and_return(faulty_response)
+        allow(Oj).to receive(:load).and_raise(Oj::ParseError.new('Unexpected character'))
+      end
+
+      it 'raises a parsing error for POST request' do
+        expect do
+          connection.cloudways_api_post_connection(post_params)
+        end.to raise_error(/Error parsing response: Unexpected character/)
       end
     end
   end
